3dc1a639bf365b24ee3befacac138ed4de6af93e
Mostafa Mohamad <businessmostafa13@gmail.com>
2025-08-21 20:33:54 +0300

    Enhance README and update plugin registrations
    - Expanded README.md to provide detailed project overview, technologies used, prerequisites, quick start guide, project structure, development notes, testing instructions, contributing guidelines, and license information.
    - Updated GeneratedPluginRegistrant.swift to reflect changes in plugin registration for Firebase Analytics and Firebase Remote Config.
    - Updated pubspec.lock to upgrade various dependencies, including Firebase packages, Flutter packages, and other transitive dependencies to their latest versions.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,9 +15,9 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
+  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
